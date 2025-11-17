91c37b169e55139632dc0d7c3664970e970735f1
pomimon <bright.dusk1649@fastmail.com>
2025-11-17 11:21:14 -0800

    Add streams data

---
+++ src/app/Data/Streams.tsx
@@ -0,0 +1,34 @@
+export const AVAILABLE_STREAMS = [
+  {
+    url: "https://www.youtube.com/embed/kkYybcn5VoM?si=OGCOzyueav5_Exw6",
+    name: "Coral Predators",
+    category: "Aquatic",
+  },
+  {
+    url: "https://www.youtube.com/embed/BLvr4K2eiRE?si=eEnDDSy07s4Mbwm1",
+    name: "Shark lagoon",
+    category: "Aquatic",
+  },
+  {
+    url: "https://www.youtube.com/embed/ubwxOJ7LBPA?si=RX7-x-TpgjvT_1ly",
+    name: "Reef lagoon",
+    category: "Aquatic",
+  },
+  {
+    url: "https://www.youtube.com/embed/w3LjpFhySTg?si=_tK4iRPsThBT6ygm",
+    name: "Kelp forest",
+    category: "Birds",
+  },
+  {
+    url: "https://www.youtube.com/embed/dzmJXWmA2EM?si=fhkrcZ-9itKGN6du",
+    name: "Spider Crab",
+    category: "Birds",
+  },
+  {
+    url: "https://www.youtube.com/embed/yuhnCtTPtZo?si=0mnkM2mdpTsMFvhT",
+    name: "Blue Cavern",
+    category: "Pets",
+  },
+];
+
+// Scrape webpages for links
