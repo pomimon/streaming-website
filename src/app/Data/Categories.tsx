ed6233cf9cb79f34187c094a9a4a92383135dee1
pomimon <bright.dusk1649@fastmail.com>
2025-11-17 11:20:44 -0800

    Add category data

---
+++ src/app/Data/Categories.tsx
@@ -0,0 +1,47 @@
+export const AVAILABLE_CATEGORIES = [
+  {
+    name: "African Wildlife",
+    image: "https://mangolinkcam.com/img/wildlife.jpg",
+  },
+  {
+    name: "Birds",
+    image: "https://mangolinkcam.com/img/birds.jpg",
+  },
+  {
+    name: "Aquatic",
+    image: "https://mangolinkcam.com/img/aquatic.jpg",
+  },
+  {
+    name: "Eagles",
+    image: "https://mangolinkcam.com/img/eagle.jpg",
+  },
+  {
+    name: "Mammals",
+    image: "https://mangolinkcam.com/img/mammals.jpg",
+  },
+  {
+    name: "Reptiles",
+    image: "https://mangolinkcam.com/img/reptiles.jpg",
+  },
+  {
+    name: "Zoos",
+    image: "https://mangolinkcam.com/img/zoos.jpg",
+  },
+
+  {
+    name: "Countries",
+    image: "https://mangolinkcam.com/img/countries.jpg",
+  },
+  {
+    name: "World",
+    image: "https://mangolinkcam.com/img/places.jpg",
+  },
+  {
+    name: "Insects",
+    image: "https://mangolinkcam.com/img/insects.jpg",
+  },
+  {
+    name: "Pets",
+    image: "https://mangolinkcam.com/img/pets.jpg",
+  },
+];
