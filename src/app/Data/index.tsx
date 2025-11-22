f45cddc6d19fd08202b8fc8f043842662479458c
pomimon <bright.dusk1649@fastmail.com>
2025-11-22 09:35:58 -0800

    fixed types from a flat array to nested objects

--- src/app/Data/index.tsx
+++ src/app/Data/index.tsx
@@ -2,10 +2,10 @@ export { AVAILABLE_CATEGORIES } from "./Categories";
 export { VIDEO_IDS as YOUTUBE_IDS } from "./Streams/YouTube";
 
 export interface VideoIdList {
-  Aquatic: string[];
-  Birds: string[];
-  Mammals: string[];
-  Pets: string[];
-  Reptiles: string[];
-  Zoos: string[];
+  Aquatic: { [subcategory: string]: string[] };
+  Birds: { [subcategory: string]: string[] };
+  Mammals: { [subcategory: string]: string[] };
+  Pets: { [subcategory: string]: string[] };
+  Reptiles: { [subcategory: string]: string[] };
+  Zoos: { [subcategory: string]: string[] };
 }
